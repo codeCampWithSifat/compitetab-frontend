4b75523b7dd8321f77b2586528bae4022b3f9ec7
codeCampWithSifat <sayedhossainsifat300@.com>
2025-06-20 16:55:03 +0600

    some code are done

--- src/components/Cart/Checkout.jsx
+++ src/components/Cart/Checkout.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router";
+import PaypalButton from "./PaypalButton";
 
 const cart = {
   products: [
@@ -33,12 +34,22 @@ const Checkout = () => {
     country: "",
     phone: "",
   });
+
+  const handleCreateCheckout = (e) => {
+    e.preventDefault();
+    setCheckoutId(123);
+  };
+
+  const handlePaymentSuccess = (details) => {
+    console.log("Payment Successfuly", details);
+    navigate("/order-confirmation");
+  };
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 max-w-7xl mx-auto py-10 px-6 tracking-tighter">
       {/* Left Section */}
       <div className="bg-white rounded-lg p-6">
         <h2 className="text-2xl uppercase mb-6">Checkout</h2>
-        <form>
+        <form onSubmit={handleCreateCheckout}>
           <h3 className="text-lg mb-4">Contact Details</h3>
           <div className="mb-4">
             <label className="block text-gray-700">Email</label>
@@ -172,10 +183,55 @@ const Checkout = () => {
             ) : (
               <div>
                 <h3 className="text-lg mb-4">Pay With Paypal</h3>
+                {/* Paypal button Component */}
+                <PaypalButton
+                  amount={500}
+                  onSuccess={handlePaymentSuccess}
+                  onError={(err) => alert("Payment Failed. Try Later", err)}
+                />
               </div>
             )}
           </div>
         </form>
+      </div>
+
+      {/* Right Section */}
+      <div className="bg-gray-50 p-6 rounded-lg ">
+        <h3 className="text-lg mb-4">Order Summary</h3>
+        <div className="bordet-t py-4 mb-4">
+          {cart.products.map((product, index) => (
+            <div
+              className="flex items-start justify-between py-2 border-b"
+              key={index}
+            >
+              <div className="flex items-start">
+                <img
+                  className="w-20 h-24 object-cover mr-4"
+                  src={product.image}
+                  alt=""
+                />
+                <div>
+                  <h3 className="text-md">{product.name}</h3>
+                  <p className="text-gray-500">Size : {product.size}</p>
+                  <p className="text-gray-500">Color : {product.color}</p>
+                </div>
+              </div>
+              <p className="text-xl"> $ {product.price?.toLocaleString()}</p>
+            </div>
+          ))}
+        </div>
+        <div className="flex justify-between items-center text-lg mb-4">
+          <p>$ {cart?.totalPrice?.toLocaleString()}</p>
+        </div>
+
+        <div className="flex justify-between items-center text-lg">
+          <p>Shipping</p>
+          <p>Free</p>
+        </div>
+        <div className="flex justify-between items-center text-lg mt-4 border-t pt-4">
+          <p>Total</p>
+          <p>{cart?.totalPrice?.toLocaleString()}</p>
+        </div>
       </div>
     </div>
   );
